b3a06e4a4c03d86e12c41c3deeaf2e508825a7a7
pawaris <best.pawaris.39@gmail.com>
2017-03-02 02:20:14 +0700

    Button MoreinfoComplete

---
+++ app/src/main/java/com/example/user/myfirstapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.myfirstapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    } //main method
+    public void onCickMoreInfo(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111")):
+        startActivity(intent);
+    }
+
+} //main Class
